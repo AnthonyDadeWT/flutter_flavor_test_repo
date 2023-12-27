54b8e7c7c832d479c0c04dc9c6102aa47dc86cbd
Anthony Dade <145477058+AnthonyDadeWT@users.noreply.github.com>
2023-12-27 16:40:05 -0500

    adding import

--- android/app/src/androidTest/java/com/example/testapp/MainActivityTest.java
+++ android/app/src/androidTest/java/com/example/testapp/MainActivityTest.java
@@ -4,6 +4,8 @@ import androidx.test.rule.ActivityTestRule;
 import dev.flutter.plugins.integration_test.FlutterTestRunner;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
+import com.example.test_app.MainActivity;
+
 
 @RunWith(FlutterTestRunner.class)
 public class MainActivityTest {
