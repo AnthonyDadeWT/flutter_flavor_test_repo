04ee025f3bab62e2382dfd7d59d5e0c3f0bc7376
Anthony Dade <145477058+AnthonyDadeWT@users.noreply.github.com>
2024-01-18 16:40:40 -0500

    Adding 2nd test file to test something

---
+++ integration_test/test2.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:test_app/main.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('test 2', () {
+    testWidgets('test 2', (tester) async {
+      await tester.pumpWidget(const MyApp());
+      // Adding this sleep here to allow test to run on Test Labs a little longer
+      await Future.delayed(Duration(seconds: 5));
+      expect(find.text("Flutter Demo Home Page"), findsOneWidget);
+    });
+  });
+}
