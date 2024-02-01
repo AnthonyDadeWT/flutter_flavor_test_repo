c3c826b66c535f08a570aa86ef1d5e6e9818048c
Anthony Dade <145477058+AnthonyDadeWT@users.noreply.github.com>
2024-02-01 09:20:59 -0500

    Adding test differences

--- integration_test/test.dart
+++ integration_test/test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:test_app/main.dart';
@@ -10,8 +11,13 @@ void main() {
         (tester) async {
       await tester.pumpWidget(const MyApp());
       // Adding this sleep here to allow test to run on Test Labs a little longer
-      await Future.delayed(Duration(seconds: 3));
+      await Future.delayed(const Duration(seconds: 3));
       expect(find.text("Flutter Demo Home Page"), findsOneWidget);
+      Finder button = find.byKey(const Key('incrementButton'));
+      await tester.tap(button);
+      await tester.tap(button);
+      await tester.pumpAndSettle();
+      expect(find.text("2"), findsOneWidget);
     });
   });
   group('end-to-end test2', () {
@@ -19,8 +25,14 @@ void main() {
         (tester) async {
       await tester.pumpWidget(const MyApp());
       // Adding this sleep here to allow test to run on Test Labs a little longer
-      await Future.delayed(Duration(seconds: 3));
+      await Future.delayed(const Duration(seconds: 3));
       expect(find.text("Flutter Demo Home Page"), findsOneWidget);
+      Finder button = find.byKey(const Key('incrementButton'));
+      await tester.tap(button);
+      await tester.tap(button);
+      await tester.tap(button);
+      await tester.pumpAndSettle();
+      expect(find.text('3'), findsOneWidget);
     });
   });
 }
--- lib/main.dart
+++ lib/main.dart
@@ -117,6 +117,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
+        key: Key("incrementButton"),
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
