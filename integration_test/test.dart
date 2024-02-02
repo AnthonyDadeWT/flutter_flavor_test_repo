3b6490ca4d23a2c89b4d2799e85be409e32fd6a6
Anthony Dade <145477058+AnthonyDadeWT@users.noreply.github.com>
2024-02-02 11:29:15 -0500

    changing to 1min

--- integration_test/test.dart
+++ integration_test/test.dart
@@ -10,7 +10,7 @@ void main() {
     testWidgets('40 min test', (tester) async {
       await tester.pumpWidget(const MyApp());
       // Adding this sleep here to allow test to run on Test Labs a little longer
-      await Future.delayed(const Duration(minutes: 40));
+      await Future.delayed(const Duration(minutes: 1));
       expect(find.text("Flutter Demo Home Page"), findsOneWidget);
       Finder button = find.byKey(const Key('incrementButton'));
       await tester.tap(button);
@@ -24,7 +24,7 @@ void main() {
         (tester) async {
       await tester.pumpWidget(const MyApp());
       // Adding this sleep here to allow test to run on Test Labs a little longer
-      await Future.delayed(const Duration(seconds: 3));
+      await Future.delayed(const Duration(minutes: 1));
       expect(find.text("Flutter Demo Home Page"), findsOneWidget);
       Finder button = find.byKey(const Key('incrementButton'));
       await tester.tap(button);
