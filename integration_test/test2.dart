e4657126cc9e32507b5a1a1ebf05cbdf28444616
Anthony Dade <145477058+AnthonyDadeWT@users.noreply.github.com>
2024-02-05 10:22:51 -0500

    updating test2

--- integration_test/test2.dart
+++ integration_test/test2.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:test_app/main.dart';
@@ -9,8 +10,14 @@ void main() {
     testWidgets('test file 2', (tester) async {
       await tester.pumpWidget(const MyApp());
       // Adding this sleep here to allow test to run on Test Labs a little longer
-      await Future.delayed(Duration(seconds: 3));
       expect(find.text("Flutter Demo Home Page"), findsOneWidget);
+      Finder button = find.byKey(const Key('incrementButton'));
+      for (var i = 0; i < 10; i++) {
+        await tester.tap(button);
+        await tester.pumpAndSettle();
+      }
+      await tester.pumpAndSettle();
+      expect(find.text("10"), findsOneWidget);
     });
   });
 }
