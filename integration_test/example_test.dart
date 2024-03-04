050b430e41bbdeef2657852e7c160a46fa5f3b9a
Anthony Dade <anthonydade@gmail.com>
2024-03-04 13:42:29 -0500

    changing test

--- integration_test/example_test.dart
+++ integration_test/example_test.dart
@@ -7,17 +7,39 @@ void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   group('end-to-end test1', () {
-    testWidgets('long 500 tap test 1', (tester) async {
+    testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+      // Build our app and trigger a frame.
       await tester.pumpWidget(const MyApp());
-      // Adding this sleep here to allow test to run on Test Labs a little longer
-      expect(find.text("Flutter Demo Home Page"), findsOneWidget);
-      Finder button = find.byKey(const Key('incrementButton'));
-      for (var i = 0; i < 25; i++) {
-        await tester.tap(button);
-        await tester.pumpAndSettle();
+
+      // Verify that our counter starts at 0.
+      expect(find.text('0'), findsOneWidget);
+      expect(find.text('1'), findsNothing);
+
+      // Tap the '+' icon and trigger a frame.
+      for (int i = 0; i < 25; i++) {
+        await tester.tap(find.byIcon(Icons.add));
       }
+      await tester.pump();
+
+      // Verify that our counter has incremented.
+      expect(find.text('0'), findsNothing);
+      expect(find.text('25'), findsOneWidget);
+
+      expect(find.byKey(Key("nextPageButton")), findsOneWidget);
+      await tester.tap(find.byKey(Key("nextPageButton")));
       await tester.pumpAndSettle();
-      expect(find.text("25"), findsOneWidget);
+
+      expect(find.text("Testing demo"), findsOneWidget);
+      await tester.tap(find.byKey(Key("button")));
+      await tester.pumpAndSettle();
+
+      expect(find.text("New text assertion"), findsOneWidget);
+
+      expect(find.byKey(Key("backButton")), findsOneWidget);
+      await tester.tap(find.byKey(Key("backButton")));
+      await tester.pumpAndSettle();
+
+      expect(find.text('0'), findsOneWidget);
     });
   });
 }
--- integration_test/tests.dart
+++ integration_test/tests.dart
@@ -1,5 +1,5 @@
 import 'test2.dart' as test2;
-import 'test.dart' as test1;
+import 'example_test.dart' as test1;
 
 void main() {
   test1.main();
